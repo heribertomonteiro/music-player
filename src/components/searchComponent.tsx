e2811846ca73e6d99065776dff20dc97f4195e5e
heribertomonteiro <heribertoomonteiroo@gmail.com>
2024-09-05 19:25:42 -0300

    fix: adjustment search component

--- src/components/searchComponent.tsx
+++ src/components/searchComponent.tsx
@@ -1,10 +1,16 @@
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import { Link } from 'expo-router';
+import AntDesign  from '@expo/vector-icons/Ionicons';
 
 export default function SearchComponent() {
   return (
     <View className='flex-1 justify-start  items-center pt-20'>
-      <TextInput style={styles.input} className='border-laranja text-white' placeholder="Buscar" placeholderTextColor={'white'}/>
+      <View style={styles.input} className='border-laranja flex-row justify-between'>
+      <TextInput placeholder="Buscar" placeholderTextColor={'white'} className=' text-white w-80'/>
+      <TouchableOpacity>
+        <AntDesign name="search" size={24} color="white" />
+      </TouchableOpacity>
+      </View>
       <Link href={"/" as any}>Home</Link>
     </View>
   );
